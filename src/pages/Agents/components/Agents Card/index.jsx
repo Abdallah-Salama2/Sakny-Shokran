591c4f878cc72a3b2a8d8d4e2ab46f7dfe0390c3
Abdallah-Salama2 <afas200030@gmail.com>
2024-10-10 04:15:13 +0300

    ff

--- src/pages/Agents/components/Agents Card/index.jsx
+++ src/pages/Agents/components/Agents Card/index.jsx
@@ -8,8 +8,8 @@ const AgentCard = ({ agent }) => {
           <h5 className="card-title">{agent.name}</h5>
           <p className="card-text"><strong>Email:</strong> {agent.email}</p>
           <p className="card-text"><strong>Phone:</strong> {agent.phone}</p>
-          <p className="card-text"><strong>Agency:</strong> {agent.agency}</p>
-          <p className="card-text"><strong>Years of Experience:</strong> {agent.experience}</p>
+          
+          
         </div>
       </div>
     </div>
